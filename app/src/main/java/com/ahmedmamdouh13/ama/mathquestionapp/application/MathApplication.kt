474161092c5db83f1ec9fc5ddd6bdbd0eadeb505
ahmedmamdouh13 <ahmedmamdouh13196@gmail.com>
2021-02-12 03:06:07 +0200

    setup debugging/profiling tools.

--- app/src/main/java/com/ahmedmamdouh13/ama/mathquestionapp/application/MathApplication.kt
+++ app/src/main/java/com/ahmedmamdouh13/ama/mathquestionapp/application/MathApplication.kt
@@ -3,14 +3,30 @@ package com.ahmedmamdouh13.ama.mathquestionapp.application
 import android.app.Application
 import androidx.hilt.work.HiltWorkerFactory
 import androidx.work.Configuration
+import cn.hikyson.godeye.core.BuildConfig
+import cn.hikyson.godeye.core.GodEye
+import cn.hikyson.godeye.core.GodEyeConfig
+import cn.hikyson.godeye.core.GodEyeHelper
+import cn.hikyson.godeye.core.exceptions.UninstallException
+import cn.hikyson.godeye.core.internal.modules.methodcanary.MethodCanaryConfig
+import cn.hikyson.godeye.core.internal.modules.methodcanary.MethodsRecordInfo
+import cn.hikyson.godeye.core.monitor.AppInfoLabel
 import dagger.hilt.android.HiltAndroidApp
+import java.lang.String
 import javax.inject.Inject
+
 
 @HiltAndroidApp
 class MathApplication : Application(), Configuration.Provider {
 
     @Inject
     lateinit var workerFactory: HiltWorkerFactory
+    override fun onCreate() {
+        super.onCreate()
+
+
+
+    }
 
     override fun getWorkManagerConfiguration(): Configuration = Configuration.Builder()
         .setWorkerFactory(workerFactory)
